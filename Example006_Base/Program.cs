ddac15bdc5adac5f269e78981041586867ff1175
1ser <1seroneser@gmail.com>
2023-10-31 12:44:13 +0300

    add example006 base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1;
+int b = 8;
+int c = 12;
+int d = 4;
+int e = 2;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max - ");
+Console.WriteLine(max);
