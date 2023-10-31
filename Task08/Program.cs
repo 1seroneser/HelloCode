566f432c25a2aa388566044f071d81eb05f0bec2
1ser <1seroneser@gmail.com>
2023-10-31 16:35:06 +0300

    add task 08

---
+++ Task08/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите натуральное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= x)
+{
+    if (count % 2 == 0) Console.Write($"{count} ");
+    count++;
+}
