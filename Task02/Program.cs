57867f21382d501785dafa8604e708f20d6ce083
1ser <1seroneser@gmail.com>
2023-10-31 15:51:15 +0300

    add task 02

---
+++ Task02/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > y) Console.Write($"min - {y}, max - {x} ");
+else Console.Write($"min - {x}, max - {y} ");
