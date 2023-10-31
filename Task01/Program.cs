7d0e646419ebcc0deda9960756871847b5b16622
1ser <1seroneser@gmail.com>
2023-10-31 15:21:03 +0300

    add task01

---
+++ Task01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (y * y == x)
+{
+    Console.WriteLine("Да");
+}
+Console.WriteLine("Нет");
