d25270d499a9a50510554bde605c14308e37aaaf
1ser <1seroneser@gmail.com>
2023-10-31 12:35:05 +0300

    add example 005 ifelse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string name = Console.ReadLine();
+if (name.ToLower() == "максим")
+{
+    Console.WriteLine("АААА МАКСИМ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(name);
+}
