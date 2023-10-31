d4b001682b347550eb22f06b9d53edf1cb247ff0
1ser <1seroneser@gmail.com>
2023-10-31 12:21:35 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 1;
-int numberB = 2;
+﻿int numberA = new Random().Next(1, 11);
+int numberB = new Random().Next(1, 11);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
