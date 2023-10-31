3e10d67e48359b0fee2fa411d7ecb3575688e64e
1ser <1seroneser@gmail.com>
2023-10-31 15:47:42 +0300

    add task 007

---
+++ Task07/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 0) x *= -1;
+if (x >= 100 && x <= 999)
+{
+    Console.Write(x % 10);
+}
+else Console.Write("Число некорректно");
