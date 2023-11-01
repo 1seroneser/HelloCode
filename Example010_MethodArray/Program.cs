db46624f6995b45400b52f8f68632d225fe2698d
1ser <1seroneser@gmail.com>
2023-11-01 17:18:10 +0300

    add Example 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {312, 123, 21, 342, 4536, 4256, 4526, 645, 67, 23, 342, 67, 45, 234};
+
+int n = array.Length;
+int find = 4526;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
