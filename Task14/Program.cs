382466f42b2c3b62055c2ff82dfcad41cf5a833f
1ser <1seroneser@gmail.com>
2023-11-11 14:46:33 +0300

    Add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+string check = Checking(x);
+
+Console.WriteLine(check);
+
+string Checking(int num)
+{
+    string answer = "No";
+    if (num % 7 == num % 23) answer = "Yes";
+    return answer;
+}
