b725bc3b37f930099f93f9fd1dc5a79961c7b37d
1ser <1seroneser@gmail.com>
2023-11-11 15:39:28 +0300

    Add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int showThirdDigit = ShowThirdDigit(number);
+
+Console.WriteLine(showThirdDigit <= 9 ? showThirdDigit : "Nothing");
+
+int ShowThirdDigit(int num)
+{
+    // if (num <= 99) num = 10;
+    // else num = (num % 1000) / 100;
+    // return num;
+    return num <= 99 ? num = 10 : num = (num % 1000) / 100;
+}
