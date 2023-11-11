7a3781befe8a62f2d12b44dae2670591c3fffee2
1ser <1seroneser@gmail.com>
2023-11-11 15:40:31 +0300

    Add Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckSquareTwoNumbers(x, y);
+
+Console.WriteLine(result ? "Yes" : "No");
+
+bool CheckSquareTwoNumbers(int firstNum, int secondNum)
+{
+    return firstNum * firstNum == secondNum || secondNum * secondNum == firstNum;
+}
