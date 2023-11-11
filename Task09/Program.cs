dbbae084b9961c009dff655d942040cd96e56cda
1ser <1seroneser@gmail.com>
2023-11-11 13:50:13 +0300

    Add Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = new Random().Next(10, 100);
+// int a = number / 10;
+// int b = number % 10;
+
+Console.WriteLine($"Случайное число в диапозоне 10 - 99: {number}");
+
+// if (a > b) Console.WriteLine($"Наибольшая цифра числа: {a}");
+// else Console.WriteLine($"Наибольшая цифра числа: {b}");
+
+// int result = a > b ? a : b;
+// Console.WriteLine($"Наибольшая цифра числа: {result}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа: {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
