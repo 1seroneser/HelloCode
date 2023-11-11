b2a1ddbafb4790266e19b2490c801350189b3173
1ser <1seroneser@gmail.com>
2023-11-11 15:15:32 +0300

    Add Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число в диапозоне 100 - 999: {number}");
+
+int showSecondDigit = ShowSecondDigit(number);
+Console.WriteLine($"Вторая цифра числа: {showSecondDigit}");
+
+int ShowSecondDigit(int num)
+{
+    return (num / 10) % 10;;
+}
