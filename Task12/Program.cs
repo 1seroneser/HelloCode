4db4682fd84e9888041703df324f6c695c85fd8d
1ser <1seroneser@gmail.com>
2023-11-11 14:12:16 +0300

    Add Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int remainds = Remainder(x, y);
+
+// if (remainds == 0) Console.WriteLine("Кратно");
+// else Console.WriteLine($"Не кратно, остаток {remainds}");
+
+Console.WriteLine(remainds == 0 ? "Кратно" : $"Не кратно, остаток {remainds}");
+
+int Remainder (int first, int second)
+{
+    return first % second;
+}
