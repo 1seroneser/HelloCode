298cec100653d79f900fe7cfd305967540380500
1ser <1seroneser@gmail.com>
2023-11-11 14:00:35 +0300

    Add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число в диапозоне 100 - 999: {number}");
+
+int removeSecondDigit = RemoveSecondDigit(number);
+Console.WriteLine($"Число без второй цифры: {removeSecondDigit}");
+
+int RemoveSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    return firstDigit * 10 + thirdDigit;
+}
